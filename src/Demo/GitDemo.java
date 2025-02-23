922514a149091e51755d059feb24e3b570489f7b
AkashCPG31 <cakashp3112@gmail.com>
2025-02-23 23:28:58 +0530

    Update GitDemo.java

--- src/Demo/GitDemo.java
+++ src/Demo/GitDemo.java
@@ -2,7 +2,7 @@ package Demo;
 
 public class GitDemo {
 	public static void main(String[] args) {
-		System.out.println("Hii"); 
+		System.out.println("Hii , how are you at git"); 
 		System.out.println("Hello"); 
 
 	}
